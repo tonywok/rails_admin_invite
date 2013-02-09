7e3cd3d0f1870079f3b8a5b10d405929ec0a8767
Tony Schneider <tonywok@gmail.com>
2013-02-09 00:17:09 -0500

    Add dependencies to gemspec

--- lib/rails_admin_invite.rb
+++ lib/rails_admin_invite.rb
@@ -4,16 +4,6 @@ module RailsAdminInvite
 end
 
 require 'rails_admin/config/actions'
-require 'rails_admin/config/sections'
-
-module RailsAdmin
-  module Config
-    module Sections
-      class Invite < RailsAdmin::Config::Sections::Edit
-      end
-    end
-  end
-end
 
 module RailsAdmin
   module Config
@@ -44,7 +34,12 @@ module RailsAdmin
               #TODO: find a way to get to AR class from @abstract_model
               @object = Admin.invite!(params[:admin], current_user)
               if @object.errors.empty?
-                redirect_to(params[:return_to], notice: I18n.t("admin.actions.invite.sent", email: @object.email))
+                notice = I18n.t("admin.actions.invite.sent", email: @object.email)
+                if params[:return_to]
+                  redirect_to(params[:return_to], notice: notice)
+                else
+                  redirect_to(invite_path(:model_name => @abstract_model.to_param, :id => @object.id), notice: notice)
+                end
               else
                 handle_save_error
               end
--- rails_admin_invite.gemspec
+++ rails_admin_invite.gemspec
@@ -1,9 +1,7 @@
 $:.push File.expand_path("../lib", __FILE__)
 
-# Maintain your gem's version:
 require "rails_admin_invite/version"
 
-# Describe your gem and declare its dependencies:
 Gem::Specification.new do |s|
   s.name        = "rails_admin_invite"
   s.version     = RailsAdminInvite::VERSION
@@ -16,4 +14,6 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.md"]
 
   s.add_dependency "rails", "~> 3.2.11"
+  s.add_dependency "rails_admin", "~> 0.4.0"
+  s.add_dependency "devise_invitable", "~> 1.0"
 end
